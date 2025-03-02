472135c0abd3de0159480a0229d5dff395a89dad
SGuarnizo <sguarniz@chicagobooth.edu>
2025-03-02 06:43:47 +0000

    board

---
+++ app/models/board.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  has_many(:posts, :class_name => "Post", :foreign_key => "board_id")
+  has_many(:active_posts, -> { where("expires_on > ?", Date.today) }, class_name: "Post", foreign_key: :board_id)
+  has_many(:expired_posts, -> { where("expires_on <= ?", Date.today) }, class_name: "Post", foreign_key: :board_id)
+end
